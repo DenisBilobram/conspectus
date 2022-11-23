b1f7cdd3b6ea51f60cce37245f99db4948731b40
DenisBilobram <denis.bilobram@mail.ru>
2022-11-23 14:46:13 +0300

    x

--- java/prac/prac/Main.java
+++ java/prac/prac/Main.java
@@ -4,6 +4,24 @@ import java.util.ArrayList;
 
 public class Main {
     public static void main(String[] args) {
-        
+        final short a = 5;
+        final short b = 10;
+        short l = a + b;
     }
+}
+
+class Person {
+    private int a = 5;
+
+    Person() {
+        this.a = 10;    
+    }
+
+    int getA() {
+        return this.a;
+    }
+}
+
+class Student extends Person {
+
 }
